186cd4ecb4925617301f228272df325f8965bd82
yuping <syp0601@126.com>
2012-06-20 19:08:23 +0800

    建立工程

---
+++ SYPFrameWork/SYPFrameWork.h
@@ -0,0 +1,13 @@
+//
+//  SYPFrameWork.h
+//  SYPFrameWork
+//
+//  Created by 玉平 孙 on 12-6-20.
+//  Copyright (c) 2012年 RenRen.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SYPFrameWork : NSObject
+-(void)TestShow;
+@end
---
+++ SYPFrameWork/SYPFrameWork.m
@@ -0,0 +1,26 @@
+//
+//  SYPFrameWork.m
+//  SYPFrameWork
+//
+//  Created by 玉平 孙 on 12-6-20.
+//  Copyright (c) 2012年 RenRen.com. All rights reserved.
+//
+
+#import "SYPFrameWork.h"
+
+@implementation SYPFrameWork
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)TestShow
+{
+    NSLog(@"TestFramework");
+}
+@end
